7e0f0d617da9fb44051ce9ed4a2ad7a04de622ef
Saputra Ari Wijaya <realary27@gmail.com>
2026-03-19 08:56:02 +0800

    adjust handling error for global pattern

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -7,11 +7,11 @@ module Authenticatable extend ActiveSupport::Concern
   def authenticate_user
     token, _ = ActionController::HttpAuthentication::Token.token_and_options(request)
 
-    return render_error("Token not provided", status: :unauthorized) if token.blank?
+    raise UnauthorizedError, "Token not provided" if token.blank?
 
     user_id = AuthenticationTokenService.decode(token)
 
-    return render_error("Invalid token", status: :unauthorized) if user_id.blank?
+    raise UnauthorizedError, "Invalid token" if user_id.blank?
 
     @current_user = User.find(user_id)
   end
